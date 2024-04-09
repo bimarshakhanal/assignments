19cab461a0040666023fd08cd32cab0d392e0911
Bimarsha Khanal <bimarsha@fusemachines.com>
2024-04-09 14:42:28 +0545

    fix: Add division function

--- mathFunctions.py
+++ mathFunctions.py
@@ -10,11 +10,15 @@ def multiplication(a,b):
     '''Function to find product of two numbers'''
     return a*b
 
+def division(a,b):
+    '''Function to perform division of two numbers'''
+    return a/b
+
 if __name__=="__main__":
     print("*"*5,"Math Operation","*"*5)
-    print("1. Addition\n2. Subtraction\n3. Mulitplication\n4. Exit")
+    print("1. Addition\n2. Subtraction\n3. Mulitplication\n4. Division\n5. Exit")
     ch = input("Enter your choice: ")
-    if ch=='4':
+    if ch=='5':
         quit()
         
     a = int(input("Enter first number: "))
@@ -29,5 +33,8 @@ if __name__=="__main__":
     elif ch=='3':
         result = multiplication(a,b)
         print("Product is ",result)
+    elif ch=='4':
+        result = division(a,b)
+        print("Result is ",result)
     else:
         print("Invalid Input")
